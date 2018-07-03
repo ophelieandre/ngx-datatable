973a3f6875cabf225a2813892f691877debcfb78
jgrenat <jgrenat@users.noreply.github.com>
2018-07-03 10:53:01 +0200

    Fix #1313 do not collapse row details when row updated

--- src/components/datatable.component.ts
+++ src/components/datatable.component.ts
@@ -373,7 +373,15 @@ export class DatatableComponent implements OnInit, DoCheck, AfterViewInit {
    *
    * (`fn(x) === fn(y)` instead of `x === y`)
    */
-  @Input() rowIdentity: (x: any) => any = ((x: any) => x);
+  @Input() rowIdentity: (x: any) => any = ((x: any) => {
+    if (this._groupRowsBy) {
+      // each group in groupedRows are stored as {key, value: [rows]},
+      // where key is the groupRowsBy index
+      return x.key;
+    } else {
+      return x;
+    }
+  });
 
   /**
    * Row specific classes.
